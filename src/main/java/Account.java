5b11f18f7541048f7b3001ad4841aa56d3822d00
GenesisDN <2033819@student.uma.pt>
2022-03-04 13:16:59 +0100

    Sistema Bancario -- Falta os teste

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,3 +1,20 @@
 public class Account {
+    private double saldo;
+
+    public Account(double saldo){
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public double depositar(double quantidade){
+        return saldo += quantidade;
+    }
+
+    public double retirar(double quantidade){
+        return saldo -= quantidade;
+    }
 
 }
--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,18 +1,60 @@
-public class Client {
-    private final String name;
-    private final int age;
-    private int numChildren;
-    private final double money;
+import java.util.AbstractCollection;
 
-    public Client (String name, int age, int numChildren, double money){
+public class Client {
+    private String name;
+    private int age;
+    private int numChildren;
+    private double quantidadeDinero;
+    private Account account;
+
+    public Client (String name, int age, int numChildren, double quantidadeDinero){
         this.age = age;
         this.name = name;
-        numChildren = 0;
-        this.money = money;
+        this.numChildren = numChildren;
+        this.quantidadeDinero = quantidadeDinero;
     }
 
     public String getName (){ return name; }
-    public double getMoney() { return money; }
+    public double getQuantidadeDinero() { return quantidadeDinero; }
     public int getAge(){ return age;}
+    public int getNumChildren() { return numChildren; }
+
+    public void setNumChildren(int numChildren) {
+        this.numChildren = numChildren;
+    }
+    public void setQuantidadeDinero(double quantidadeDinero) {
+        this.quantidadeDinero = quantidadeDinero;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean criarConta(){ //Booleano para fazer o teste mais facil
+        boolean check = false;
+        if(this.account != null){
+            Account newAccount = new Account(this.quantidadeDinero);
+            this.account = newAccount;
+            check = true;
+        }
+        return check;
+    }
+
+    public boolean emprestimos (){
+        double emprestimo = 0;
+        if (this.account.getSaldo() > 0) {
+             emprestimo += 0.45;
+        }
+        if(this.age >= 18){
+            emprestimo = 0.25;
+        }
+        if(this.numChildren < 3){
+            emprestimo = 0.30;
+        }
+        return emprestimo >= 0.60;
+    }
+
 
 }
